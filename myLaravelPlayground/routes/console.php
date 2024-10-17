597e5fa121b8db7a498d16f438f8aa2360698a37
David Dickinson <rnddave@gmail.com>
2024-10-17 12:09:18 +0100

    added some example console routes

--- myLaravelPlayground/routes/console.php
+++ myLaravelPlayground/routes/console.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('hello', function () {
+    $this->info('Hello, World!');
+})->purpose('Display a greeting');
+
+Artisan::command('progress:demo', function () {
+    $bar = $this->output->createProgressBar(100);
+    $bar->start();
+    for ($i = 0; $i < 100; $i++) {
+        usleep(50000);
+        $bar->advance();
+    }
+    $bar->finish();
+    $this->newLine();
+    $this->info('Task completed!');
+})->purpose('Demonstrate progress bar');
