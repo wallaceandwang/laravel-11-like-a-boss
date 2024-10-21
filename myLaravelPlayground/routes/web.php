bf63655d82e3122e43a7e441b2c5a2c0868e82ad
David Dickinson <rnddave@gmail.com>
2024-10-21 12:22:39 +0100

    working on the index and routes

--- myLaravelPlayground/routes/web.php
+++ myLaravelPlayground/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login-page', function () {
+    return 'We will build the Sign-Up / Sign-In stuff here';
+});
+
+Route::get('/404', function () {
+    return 'We might build a custom 404, or something else, etc';
+});
